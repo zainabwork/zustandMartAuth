39e73a937a8cf3af1330ae5956f3129f8b2c1ae5
zainab rasheed <zainab-rasheed@netixsol.com>
2024-06-04 18:07:24 +0500

    removed typescript error from useAuthStore

--- store/useAuthStore.ts
+++ store/useAuthStore.ts
@@ -4,26 +4,26 @@ import axios from 'axios';
 
 interface AuthStore {
     token:string | null,
-    isAuthenticated: boolean,
+    isAuthenticated: string | null,
     login:(email:string,password:string) => void,
     logout: () => void
 }
 export const useAuthStore = create<AuthStore>((set)=>({
     token:Cookies.get('token') || null, 
-    isAuthenticated: Cookies.get("token"),
+    isAuthenticated: Cookies.get("token") || null,
     login: async(email:string, password:string) => {
         try{
             const response = await axios.post('https://reqres.in/api/login',{email,password});
             const token = response.data.token;
             Cookies.set('token',token,{expires:1});
             console.log("token:",token)
-            set({token:token,isAuthenticated:true});
+            set({token:token,isAuthenticated:"true"});
         } catch(error){
             alert('Invalid credentials');
         }
     },
     logout: () => {
         Cookies.remove('token');
-        set({token:null,isAuthenticated:false});
+        set({token:null,isAuthenticated:"false"});
     },
 }));
